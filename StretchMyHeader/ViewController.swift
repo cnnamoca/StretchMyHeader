cf47704a1300f018367bc917f4d29e08c83c02e4
Carlo <carlo.namoca@gmail.com>
2017-11-07 11:08:09 -0800

    Initial Commit

---
+++ StretchMyHeader/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StretchMyHeader
+//
+//  Created by Carlo Namoca on 2017-11-07.
+//  Copyright © 2017 Carlo Namoca. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
